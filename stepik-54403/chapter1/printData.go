88284ba5df06c3266434570ef36e4d1b7433db40
re1nor <zamaletdinovbulat@yandex.ru>
2023-11-14 22:51:35 +0300

    Print Data
    Print() and Println() fuctions

---
+++ stepik-54403/chapter1/printData.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Hello, Go")
+	fmt.Print("Hello, Go")
+
+	fmt.Println("Hello,Go")
+	fmt.Print("Hello,Go")
+
+	fmt.Println("Bulat", 21)
+	fmt.Print("Bulat", 21)
+	fmt.Print(26, 21)
+
+	name := "Bulat"
+	age := 21
+	fmt.Println("My name is", name, "and I am", age, "years old,")
+}
