db8bdaf85466d5cc99c63873e5024761526a38cb
re1nor <zamaletdinovbulat@yandex.ru>
2023-12-05 12:49:35 +0300

    Iota vol1

---
+++ stepik-54403/chapter1/day2/iota1.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+const (
+	Sunday = iota
+	Monday
+	Tuesday
+	Wednesday
+	Thursday
+	Friday
+	Saturday
+)
+
+func main() {
+	fmt.Println(Sunday)
+	fmt.Println(Monday)
+	fmt.Println(Saturday)
+}
