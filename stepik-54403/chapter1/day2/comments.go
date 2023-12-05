6cd21d50d2b9efe4214c058e8294621b4ad070ea
re1nor <zamaletdinovbulat@yandex.ru>
2023-12-05 12:49:01 +0300

    Comments

---
+++ stepik-54403/chapter1/day2/comments.go
@@ -0,0 +1,15 @@
+/*
+	Comments, comments,
+	comments.
+*/
+
+package main // определение пакета для текущего файла
+
+import "fmt" // подключение пакета fmt
+
+// определение функции mainss
+func main() {
+
+	fmt.Println("Go comments")
+	fmt.Println( /*comment*/ "I like Go!")
+}
